ed65a119826ae66ee160d07485307a2580a5563a
221b415 <129018904+tm3390@users.noreply.github.com>
2025-01-16 12:07:50 +0530

    Update Main.java

--- Lab-1/Main.java
+++ Lab-1/Main.java
@@ -1,6 +1,22 @@
-public class Main
-{
+public class Main {
+	private String name; 
+	private int age; 
+	public String getName() 
+	{ return name;
+	} 
+	public void setName(String name) 
+	{ this.name = name; }
+	public int getAge() { 
+		return age; 
+	} 
+	public void setAge(int age) {
+		this.age = age; 
+	} 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+		Main person = new Main(); 
+	person.setName("John Doe"); 
+	person.setAge(30); 
+	System.out.println("Name: " + person.getName()); 
+	System.out.println("Age: " + person.getAge()); 
+	} 
 }
